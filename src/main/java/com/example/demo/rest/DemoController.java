8b8b3cd37ebede726d45873f60d47034eeb6dac3
LucasMacieel <lucasmacielsousa29901@gmail.com>
2025-07-19 19:09:46 -0300

    Added custom properties

--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -1,10 +1,17 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -16,8 +23,8 @@ public class DemoController {
         return "Run a hard 5k!";
     }
 
-    @GetMapping("/fortune")
-    public String getDailyFortune() {
-        return "Today is your lucky day.";
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return String.format("Coach: %s, Team Name: %s", coachName, teamName);
     }
 }
