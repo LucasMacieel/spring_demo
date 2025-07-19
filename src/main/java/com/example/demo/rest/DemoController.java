7e745403616b9244f5eda507bc650ea2f91ad586
LucasMacieel <lucasmacielsousa29901@gmail.com>
2025-07-19 11:51:41 -0300

    Added devtools dependency and more endpoints

--- src/main/java/com/example/demo/rest/DemoController.java
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping
+    @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
     }
 }
