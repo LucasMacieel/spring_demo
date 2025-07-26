e8303524ed5cf0028e54f326a31cfbc083463b98
LucasMacieel <lucasmacielsousa29901@gmail.com>
2025-07-26 14:47:01 -0300

    Added bean lifecycle methods examples

--- src/main/java/com/example/demo/common/CricketCoach.java
+++ src/main/java/com/example/demo/common/CricketCoach.java
@@ -1,10 +1,22 @@
 package com.example.demo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 // @Component - Marks a class to be managed by Spring, being called as a "bean".
 @Component
 public class CricketCoach implements Coach {
+
+    /*@PostConstruct - Called after bean creation.
+    public void startUp() {
+        System.out.println("In startUp(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy - Called after bean destruction.
+    public void cleanUp() {
+        System.out.println("In cleanUp(): " + getClass().getSimpleName());
+    }*/
 
     @Override
     public String getDailyWorkout() {
