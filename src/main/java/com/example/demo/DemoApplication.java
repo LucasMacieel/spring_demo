7c303e857063d5b16d017c13f1cd614dba626c23
LucasMacieel <lucasmacielsousa29901@gmail.com>
2025-08-13 19:41:33 -0300

    Added querying objects examples

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -17,45 +19,61 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			readStudent(studentDAO);
+			queryForStudentsByLastName(studentDAO);
 		};
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
 		System.out.println("Creating 3 student objects...");
-		Student tempStudent1 = new Student("John", "Doe", "paul@gmail.com");
-		Student tempStudent2 = new Student("Mary", "Public", "mary@gmail.com");
-		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@gmail.com");
+		Student student1 = new Student("John", "Doe", "paul@gmail.com");
+		Student student2 = new Student("Mary", "Public", "mary@gmail.com");
+		Student student3 = new Student("Bonita", "Applebum", "bonita@gmail.com");
 
 		System.out.println("Saving the students...");
-		studentDAO.save(tempStudent1);
-		studentDAO.save(tempStudent2);
-		studentDAO.save(tempStudent3);
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
 		System.out.println("Creating new student object...");
-		Student tempStudent = new Student("Paul", "Doe", "paul@gmail.com");
+		Student student = new Student("Paul", "Doe", "paul@gmail.com");
 
 		System.out.println("Saving the student...");
-		studentDAO.save(tempStudent);
+		studentDAO.save(student);
 
-		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+		System.out.println("Saved student. Generated id: " + student.getId());
 	}
 
 	private void readStudent(StudentDAO studentDAO) {
 		System.out.println("Creating new student object...");
-		Student tempStudent = new Student("Daffy", "Duck", "daffy@gmail.com");
+		Student student = new Student("Daffy", "Duck", "daffy@gmail.com");
 
 		System.out.println("Saving the student...");
-		studentDAO.save(tempStudent);
+		studentDAO.save(student);
 
-		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+		System.out.println("Saved student. Generated id: " + student.getId());
 
-		System.out.println("Retrieving student with id: " + tempStudent.getId());
+		System.out.println("Retrieving student with id: " + student.getId());
 
-		Student myStudent = studentDAO.findById(tempStudent.getId());
+		Student myStudent = studentDAO.findById(student.getId());
 
 		System.out.println("Find the student: " + myStudent);
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		List<Student> studentList = studentDAO.findAll();
+
+		for (Student student: studentList) {
+			System.out.println(student);
+		}
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+		List<Student> studentList = studentDAO.findByLastName("Duck");
+
+		for (Student student: studentList) {
+			System.out.println(student);
+		}
 	}
 }
--- src/main/java/com/example/demo/dao/StudentDAOImpl.java
+++ src/main/java/com/example/demo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository // Marks this implementation as a spring component and translates JDBC exceptions
 public class StudentDAOImpl implements StudentDAO {
@@ -26,5 +29,21 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student order by lastName", Student.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        query.setParameter("theData", lastName);
+
+        return query.getResultList();
     }
 }
